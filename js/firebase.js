78cf96a722f71776aa88d89f8516bb4607bca384
Melsss08 <imelllll08@gmail.com>
2024-12-27 21:23:31 +0800

    first commit

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDPayMyPr6Yr7BACUk65hO5PTdALf5J5Yg",
+    authDomain: "smartvote-8f173.firebaseapp.com",
+    databaseURL: "https://smartvote-8f173-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smartvote-8f173",
+    storageBucket: "smartvote-8f173.firebasestorage.app",
+    messagingSenderId: "900324968882",
+    appId: "1:900324968882:web:096903204f5d873b87bcab",
+    measurementId: "G-ERKXDTS8E5"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
